0945944c6530ba290f26cf199019e831ef0c70dc
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-03-12 06:09:03 +0200

    initialize project

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xff0E0E0E);
+  static const Color white = Colors.white;
+  static const Color red = Color(0XFFEE403D);
+}
---
+++ lib/ui/on_boarding_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  static const String routeName = "/onBoardingScreen";
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
